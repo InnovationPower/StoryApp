86aaa216ad75e1d85a06c1d98fe0c5f250c93df1
宮島衣瑛 <kiriem@KYRIEsMacBookAirM1.local>
2021-11-21 21:30:37 +0900

    Initial commit

---
+++ storyData.js
@@ -0,0 +1,198 @@
+const storyListDefine = [
+
+    {
+        layer: 1,
+        sceneId: 1,
+        sceneTitle: "title",
+        datas:[
+            {
+                subtitle: "subtitle",
+                text: "text",
+                imageUrl: "url"
+            },
+            {
+                subtitle: "subtitle",
+                text: "text",
+                imageUrl: "url"
+            },
+        ],
+
+        choices:[
+            {
+                choiceId: 0,
+                title: "選択肢A",
+                text: "",
+                nextSceneId: ""
+            },
+            {
+                choiceId: 1,
+                title: "",
+                text: "",
+                nextSceneId: ""
+            },
+            {
+                choiceId: 2,
+                title: "",
+                text: "",
+                nextSceneId: ""
+            },
+        ],
+    },
+
+    {
+        layer: 2,
+        sceneId: 1,
+        sceneTitle: "title",
+        datas:[
+            {
+                subtitle: "subtitle",
+                text: "text",
+                imageUrl: "url"
+            },
+            {
+                subtitle: "subtitle",
+                text: "text",
+                imageUrl: "url"
+            },
+        ],
+
+        choices:[
+            {
+                choiceId: 0,
+                title: "選択肢A",
+                text: "",
+                nextSceneId: ""
+            },
+            {
+                choiceId: 1,
+                title: "",
+                text: "",
+                nextSceneId: ""
+            },
+            {
+                choiceId: 2,
+                title: "",
+                text: "",
+                nextSceneId: ""
+            },
+        ],
+    },
+
+    {
+        layer: 2,
+        sceneId: 1,
+        sceneTitle: "title",
+        datas:[
+            {
+                subtitle: "subtitle",
+                text: "text",
+                imageUrl: "url"
+            },
+            {
+                subtitle: "subtitle",
+                text: "text",
+                imageUrl: "url"
+            },
+        ],
+
+        choices:[
+            {
+                choiceId: 0,
+                title: "選択肢A",
+                text: "",
+                nextSceneId: ""
+            },
+            {
+                choiceId: 1,
+                title: "",
+                text: "",
+                nextSceneId: ""
+            },
+            {
+                choiceId: 2,
+                title: "",
+                text: "",
+                nextSceneId: ""
+            },
+        ],
+    },
+
+    {
+        layer: 2,
+        sceneId: 1,
+        sceneTitle: "title",
+        datas:[
+            {
+                subtitle: "subtitle",
+                text: "text",
+                imageUrl: "url"
+            },
+            {
+                subtitle: "subtitle",
+                text: "text",
+                imageUrl: "url"
+            },
+        ],
+
+        choices:[
+            {
+                choiceId: 0,
+                title: "選択肢A",
+                text: "",
+                nextSceneId: ""
+            },
+            {
+                choiceId: 1,
+                title: "",
+                text: "",
+                nextSceneId: ""
+            },
+            {
+                choiceId: 2,
+                title: "",
+                text: "",
+                nextSceneId: ""
+            },
+        ],
+    },
+
+    {
+        layer: 2,
+        sceneId: 1,
+        sceneTitle: "title",
+        datas:[
+            {
+                subtitle: "subtitle",
+                text: "text",
+                imageUrl: "url"
+            },
+            {
+                subtitle: "subtitle",
+                text: "text",
+                imageUrl: "url"
+            },
+        ],
+
+        choices:[
+            {
+                choiceId: 0,
+                title: "選択肢A",
+                text: "",
+                nextSceneId: ""
+            },
+            {
+                choiceId: 1,
+                title: "",
+                text: "",
+                nextSceneId: ""
+            },
+            {
+                choiceId: 2,
+                title: "",
+                text: "",
+                nextSceneId: ""
+            },
+        ],
+    },
+
+]
